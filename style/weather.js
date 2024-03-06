310ad28d3bb08f790ba3e1f02e7b1693574c8063
RandomEnch <1048421623@qq.com>
2024-03-06 14:46:11 +0800

    Site updated: 2024-03-06 14:46:10

---
+++ style/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "7f790149f68f4e8fb7dd7913be2132dd"
+  }
+}
