545910065e5f0104e79c92d3ecda46f5be302842
RandomEnch <1048421623@qq.com>
2024-03-06 15:09:41 +0800

    Site updated: 2024-03-06 15:09:40

--- style/weather.js
+++ style/weather.js
@@ -1,11 +1,22 @@
 WIDGET = {
   "CONFIG": {
-    "layout": "2",
-    "width": 230,
-    "height": 270,
+    "modules": "01234",
     "background": "1",
-    "dataColor": "FFFFFF",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
     "borderRadius": "10",
-    "key": "e0bb37f638354db288c3c42c6f0784a9"
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "9a56ab2e46e84cc9b01e15b328e48964"
   }
 }
