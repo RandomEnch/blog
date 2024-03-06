7244683958718acc9c64b441474008f3f3a6fbe5
RandomEnch <1048421623@qq.com>
2024-03-06 15:45:28 +0800

    Site updated: 2024-03-06 15:45:27

--- style/weather.js
+++ style/weather.js
@@ -1,22 +1,12 @@
-WIDGET = {
-  "CONFIG": {
-    "modules": "01234",
-    "background": "5",
-    "tmpColor": "000000",
-    "tmpSize": "16",
-    "cityColor": "000000",
-    "citySize": "16",
-    "aqiColor": "000000",
-    "aqiSize": "16",
-    "weatherIconSize": "24",
-    "alertIconSize": "18",
-    "padding": "10px 10px 10px 10px",
-    "shadow": "0",
-    "language": "auto",
-    "borderRadius": "10",
-    "fixed": "false",
-    "vertical": "top",
-    "horizontal": "left",
-    "key": "41b47212cd844dad9c5ce6c626c4b617"
-  }
-}
+(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+window.SeniverseWeatherWidget('show', {
+  flavor: "bubble",
+  location: "WX4FBXXFKE4F",
+  geolocation: true,
+  language: "auto",
+  unit: "c",
+  theme: "auto",
+  token: "1e881efc-9864-442e-a8a1-ebbe3f49afc9",
+  hover: "enabled",
+  container: "tp-weather-widget"
+})
